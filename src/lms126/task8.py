6eb24c7eb45d22acd0c14611e137a13a13bd5a81
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 14:30:52 +0100

    solution task8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,25 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    # Start
+    # Input x
+    # If x/3=0
+    # print Foo
+    # If x/5=0
+    # print Bar
+    # If x/3=0 and x/5=0
+    # print FooBar
+    # else Qix
+    # End
+    return """
+    INPUT x is number
+    If x/3=0
+    OUTPUT Foo
+    If x/5=0
+    OUTPUT Bar
+    If x/3=0 and x/5=0
+    OUTPUT FooBar
+    If x/3=!0 and x/5=!0
+    OUTPUT Qix
+    
+    """
