94cbd641e8ea6f024a0e0b12a8cb43b85ea04aa4
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 12:43:30 +0100

    solution task5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,18 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    # Start
+    # input X
+    # if X = 1 to 8
+    # for week ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    # End
+
+    return """
+    INPUT X is number
+    INPUT X = 1 to 8
+    Week ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    OUTPUT (Week[X] is X)
+    
+    
+    
+    """
