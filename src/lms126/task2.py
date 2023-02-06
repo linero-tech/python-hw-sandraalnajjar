debda5784325f422b1368b7615a166a5260867d4
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 09:17:07 +0100

    solution task2

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -2,4 +2,10 @@ from to_do import TODO
 
 
 def task2():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT height of the rectangle
+    INPUT base of the rectangle
+    Calculate the area of the rectangle
+    OUTPUT the area of the rectangle
+    
+"""
