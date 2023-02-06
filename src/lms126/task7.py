4012be201d84b465a3990c5c09c89df3375a2f63
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 14:19:02 +0100

    solution task7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task7():
-    return TODO("Erase this line and write your solution")
+    # Start
+    # Input 100>x>1
+    # If x/2=0
+    # print x
+    # else End
+    return """
+    INPUT x is number 
+    If x/2=0
+    OUTPUT x
+    
+    """
