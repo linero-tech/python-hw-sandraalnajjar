36d752de441d3c81eac7fa27b0927e15e8f6434b
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 14:42:40 +0100

    solution task9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,13 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT insert card
+    INPUT card's password
+    INPUT insert how much money do you want to withdraw
+    ATM ask if you want to take recept
+    If yes
+    OUTPUT recept and money
+    If no 
+    OUTPUT money   
+    """
