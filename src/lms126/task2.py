af119e53a7048fc600bad33e02d38a96b10dea37
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 10:25:22 +0100

    update task2

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -1,7 +1,12 @@
-from to_do import TODO
+# from to_do import TODO'
 
 
 def task2():
+    # height = input("height of the rectangle")
+    # base  = input("base of the rectangle")
+    # area = height * base
+    # print (area)
+
     return """
     INPUT height of the rectangle
     INPUT base of the rectangle
