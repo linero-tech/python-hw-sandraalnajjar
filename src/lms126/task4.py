b3e1dde7d553e8a979124d7ecd65012d8bc7ad37
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-06 11:14:43 +0100

    solution task4

--- src/lms126/task4.py
+++ src/lms126/task4.py
@@ -2,4 +2,18 @@ from to_do import TODO
 
 
 def task4():
-    return TODO("Erase this line and write your solution")
+    # START
+    # INPUT number X
+    # INPUT number Y
+    # If X > Y
+    # OUTPUT "X"
+    # ELSE "Y"
+    # End
+    return """
+    INPUT X is number
+    INPUT Y is number 
+    IF X > Y 
+    OUTPUT X
+    ELSE Y
+    OUTPUT this number has the greater value.
+    """
