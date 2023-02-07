38a7baac42e3b6efe27fe29af53e10ef421a2f93
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-07 22:02:05 +0100

    solution task11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    # Start
+    # Input x
+    # If xC
+    # convert xf
+    # Else convert xC
+    # End
+    return """
+    INPUT x is temperature
+    If xC
+    OUTPUT convert xF
+    Else
+    OUTPUT xC
+    
+    """
