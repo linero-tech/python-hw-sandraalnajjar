bf587202cc21da94c3bfce9719f37624207bf973
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-07 22:28:56 +0100

    update task22

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -10,7 +10,7 @@ def task2():
     return """
     INPUT height of the rectangle
     INPUT base of the rectangle
-    Calculate height * base
+    Calculate the area of the rectangle
     OUTPUT the area of the rectangle
     
 """
