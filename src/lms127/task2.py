1185218be5da126b7bedcabecc9946913b9e1e16
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-15 09:52:08 +0100

    solution task2.102

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -2,4 +2,11 @@ from to_do import TODO
 
 
 def task2():
-    TODO("Erase this line and write your solution")
+    name = "Jack"
+    country = "Syria"
+    month = "August"
+    print(f"Hello {name}, you are from {country} and you were born in {month}")
+
+
+if __name__ == "__main__":
+    task2()
