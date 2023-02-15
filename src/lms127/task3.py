a09e35ff6b4787f31d15542e840d5e04a53ffb4d
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-15 11:54:02 +0100

    solution task3.102

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -1,7 +1,12 @@
-from to_do import TODO
+# from to_do import TODO
 
 
-def task3(radius):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+def task3():
+    radius = "10"
+    # perimeter of a circle =π * (2*10)
+    result = "62.8"
+    print("Assume that\t\n radius is 10\nThen\t\n result is 62.8")
+
+
+if __name__ == "__main__":
+       task3()
