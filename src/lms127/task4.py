0fb8612986dabfd801866d3827c5a618f5b97189
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-22 08:59:28 +0100

    update task4.4.102

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -1,14 +1,13 @@
 # from to_do import TODO
 
 
-def task4():
-   base = 5
-   height = 10
-   area = (base* height)/2
-   result= area
-   print(f"Assume that \t\n base is {base} \t\n height is {height} \nThen\t\n result is {result}")
+def task4(base, height):
+   area = 0.5 * base * height
+   return area
 
 
 if __name__ == "__main__":
-       task4()
+       print(task4(base=5.0,height=10.0))
+
+
 
