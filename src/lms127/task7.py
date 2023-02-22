0ca6a54b336104aa54dc13cc4d9949b66e631fa1
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-22 10:00:43 +0100

    solution task7.102

--- src/lms127/task7.py
+++ src/lms127/task7.py
@@ -2,6 +2,13 @@ from to_do import TODO
 
 
 def task7(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    variable = sentence
+    len(variable)
+    result=len(variable)
+
+    return result
+
+if __name__ == "__main__":
+    result = task7(sentence="I love GBG")
+    print(result)
+
