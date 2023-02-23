a5ec78a8adc09f79d35dae432056cf04484a5982
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-23 09:52:20 +0100

    solution task8.8.102

--- src/lms127/task8.py
+++ src/lms127/task8.py
@@ -1,7 +1,9 @@
 from to_do import TODO
-
-
+# Inline
+# Refactoring : changing the implemantation But keeping the behavior
 def task8(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return sentence.count(character)
+
+
+if __name__ == "__main__":
+    print(task8(sentence="I code in KOTLIN", character="I"))
