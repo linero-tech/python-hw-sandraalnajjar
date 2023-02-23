e07fe9c88bd429b456edb0541c92d788defb5ae5
sandraalnajjar <sandrarawad2@gmail.com>
2023-02-23 10:11:56 +0100

    solution task10

--- src/lms127/task10.py
+++ src/lms127/task10.py
@@ -1,25 +1,25 @@
 from to_do import TODO
 
-
+# Refactoring
 def task10_1(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return len(assessments)
+
+
 
 
 def task10_2(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return assessments[3]
 
 
 def task10_3(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return assessments[len(assessments) // 2]
 
 
 def task10_4(assessments):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    return assessments[0:3]
+
+if __name__ == "__main__":
+    print(task10_1(assessments="LMHHF"))
+    print(task10_2(assessments="LMFHMF"))
+    print(task10_3(assessments="LMFHM"))
+    print(task10_4(assessments="LMFHM"))
