59d63ceda4aa163331f004ce68050bc9c29a4ead
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-03 11:38:47 +0100

    solution task5.103

--- src/lms129/task5.py
+++ src/lms129/task5.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task5(sentence):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    string = "I Love GBG"
+    result = 0
+
+    for character in string:
+        result += 1
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task5("string"))
