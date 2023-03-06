8a586a02a3e34bc03472a57f558fd5fa80f1050e
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-06 14:20:16 +0100

    solution task2.103

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,6 +2,18 @@ from to_do import TODO
 
 
 def task2(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    result = True
+
+    if number > 1:
+        for i in range(2,number+1):
+            if (number % i) == 0:
+                print(False)
+            
+
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task2(5))
