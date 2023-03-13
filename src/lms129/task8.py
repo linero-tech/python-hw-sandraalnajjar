cd8378c80bc8ccd201ccaf772e7205b5ab1acf09
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-13 13:29:32 +0100

    solution task8.103

--- src/lms129/task8.py
+++ src/lms129/task8.py
@@ -2,6 +2,14 @@ from to_do import TODO
 
 
 def task8(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 0
+
+    if number > 0:
+        for i in str(number):
+            result += int(i)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task8(123))
