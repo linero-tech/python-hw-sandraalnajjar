8e180b79388ade2ea32ed19ceea0d3b43d2f5833
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-13 14:00:45 +0100

    solution task2.103

--- src/lms129/task2.py
+++ src/lms129/task2.py
@@ -2,15 +2,14 @@ from to_do import TODO
 
 
 def task2(number):
-
     result = True
 
     if number > 1:
-        for i in range(2,number+1):
+        for i in range(2, number + 1):
             if (number % i) == 0:
                 print(False)
-            else:
-                return result
+
+            return result
 
 
 if __name__ == "__main__":
