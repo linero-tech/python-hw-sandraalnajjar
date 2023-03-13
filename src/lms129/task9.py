568b9ef45b70a859c6532004251b3cf0faa08d91
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-13 15:36:35 +0100

    solution task9.103

--- src/lms129/task9.py
+++ src/lms129/task9.py
@@ -1,14 +1,19 @@
- from to_do import TODO
+from to_do import TODO
 
 
 def task9(temperature):
-    magnitude = 0
-    unit= ""
+    magnitude= 50
+    unit = "F"
+    temperature= 50f
 
-    if unit is in celsius
-    else:
-      #convert to celsius
+
+    result = str(int((temperature[-1].upper()) == (temperature * 9 / 5) + 32)) + "C"
+
+    if temperature[-1].upper() == "C":
+        result = str(int((temperature[-1].upper()) / 5 == (temperature - 32) / 9)) + "F"
+
     return result
 
+
 if __name__ == "__main__":
-    print(task9(15c))
+    print(task9(temperature is 50F))
