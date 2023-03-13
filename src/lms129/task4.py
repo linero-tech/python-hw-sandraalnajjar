90802a6d93903d6db9fccd1886ad97a2e1efd9bc
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-13 13:40:50 +0100

    solution task4.103

--- src/lms129/task4.py
+++ src/lms129/task4.py
@@ -3,10 +3,11 @@ from to_do import TODO
 
 def task4():
     result = 0
-    for i in range(1,1000):
+    for i in range(1, 1000):
         if i % 9 == 0:
             result += i
     return result
 
+
 if __name__ == "__main__":
-       print(task4())
+    print(task4())
