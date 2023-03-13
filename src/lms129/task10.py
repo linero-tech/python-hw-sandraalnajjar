18e50a20c6baffa44f094f1bf12e5cca51b44afb
sandraalnajjar <sandrarawad2@gmail.com>
2023-03-13 13:30:00 +0100

    solution task10.103

--- src/lms129/task10.py
+++ src/lms129/task10.py
@@ -1,7 +1,18 @@
+import re
+
 from to_do import TODO
+import re
 
 
 def task10(password):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    has_lowercase = len(re.findall("[a-z]", password)) > 0
+    has_special_character = len(re.findall("[$#@]", password)) > 0
+    has_uppercase = len(re.findall("[A-Z]", password)) > 0
+    has_number = len(re.findall("[0-9]", password)) > 0
+    has_correct_length = len(password) in range(6, 11)
+
+    return has_lowercase and has_special_character and has_uppercase and has_number and has_correct_length
+
+
+if __name__ == "__main__":
+    print(task10(password="San$991"))
